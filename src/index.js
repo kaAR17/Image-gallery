fc2c68630ca81d0e5bba4474f0a3bf7d86d0ad67
kaAR17 <vanithasure14@gmail.com>
2025-03-08 08:28:58 +0530

    update project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './style.css';
+import dog1 from './assets/images/dog1.jpg';
+import dog2 from './assets/images/dog2.jpg';
+import dog3 from './assets/images/dog3.jpg';
+import dog4 from './assets/images/dog4.jpg';
+import dog5 from './assets/images/dog5.jpg';
+import dog6 from './assets/images/dog6.jpg';
+import dog7 from './assets/images/dog7.jpg';
+import dog8 from './assets/images/dog8.jpg';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Gallery(props) {
+  return (
+    <div style={{ textAlign: "center", margin: "20px" }}>
+      
+      <img 
+        src={props.image} 
+        alt="Gallery"
+        style={{
+          width: "200px",
+          height: "200px",
+          display: "block",
+          margin: "auto",
+          padding: "10px",
+          borderRadius: "10px" }} 
+          />
+<h2>{props.Name}</h2>
+       
+      
+    </div>
+  );
+}
+
+const arr = [
+  { image: dog1, Name: "Labrador" },
+  { image: dog2, Name: "Bulldog" },
+  { image: dog3, Name: "Doberman" },
+  { image: dog4, Name: "German Shepherd" },
+  { image: dog5, Name: "Beagle" },
+  { image: dog6, Name: "Poodle" },
+  { image: dog7, Name: "Dachshund" },
+  { image: dog8, Name: "Pug" }
+];
+
+root.render(
+  <div>
+    <h1 style={{ textAlign: "center" }}>Image Gallery</h1>
+    <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
+      {arr.map((item, index) => (
+        <Gallery key={index} image={item.image} Name={item.Name} />
+      ))}
+    </div>
+  </div>
+);
